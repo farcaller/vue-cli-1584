035037e9942356c8896a24379841a1ac7486bf7f
Vladimir Pouzanov <farcaller@gmail.com>
2019-01-30 14:16:45 +0000

    Breaks here

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,6 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
+import '@/plugins/vuetify'
+
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
